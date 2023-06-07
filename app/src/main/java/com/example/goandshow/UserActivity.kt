4c64929be1b1629a7c735ddf87d1df3238c8d0e2
StevenLiang <1950225147@qq.com>
2023-06-07 10:16:46 +0800

    commits

--- app/src/main/java/com/example/goandshow/UserActivity.kt
+++ app/src/main/java/com/example/goandshow/UserActivity.kt
@@ -1,11 +1,24 @@
 package com.example.goandshow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class UserActivity : AppCompatActivity() {
+
+    private lateinit var logoutButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        logoutButton = findViewById(R.id.logoutButton)
+
+        logoutButton.setOnClickListener {
+            val intent = Intent(this, CoverActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
